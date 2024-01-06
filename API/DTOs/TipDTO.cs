eed8605b66bda593eff2837fcf9cdafc177412e2
adomas-vensas <adoma@DESKTOP-09F4KST>
2024-01-07 01:48:08 +0200

    - Added Tip

---
+++ API/DTOs/TipDTO.cs
@@ -0,0 +1,16 @@
+﻿using API.Enumerators;
+using API.Model;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs
+{
+    public class TipDTO
+    {
+        [Required]
+        public Price Price { get; set; }
+        [Required]
+        public PaymentType PaymentType { get; set; }
+        [Required]
+        public TipType TipType { get; set; }
+    }
+}
---
+++ API/Model/Tip.cs
@@ -0,0 +1,11 @@
+﻿using API.Enumerators;
+
+namespace API.Model
+{
+    public class Tip
+    {
+        public Price Price { get; set; }
+        public PaymentType PaymentType { get; set; }
+        public TipType TipType { get; set; }
+    }
+}
