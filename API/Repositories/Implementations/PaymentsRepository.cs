4d70b07ac410534b4cc2941cbd0c898f03c69126
adomas-vensas <adoma@DESKTOP-09F4KST>
2024-01-06 12:29:50 +0200

    - added IPaymentsRepository interface - added PaymentsRepository class

---
+++ API/Repositories/Implementations/PaymentsRepository.cs
@@ -0,0 +1,33 @@
+﻿using API.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Repositories.Implementations
+{
+    public class PaymentsRepository : IPaymentsRepository
+    {
+        public IActionResult CreatePayment()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult DeletePayment(Guid paymentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult GetPayment(Guid paymentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult GetPayments()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult UpdatePayment(Guid paymentId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ API/Repositories/Interfaces/IPaymentsRepository.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Repositories.Interfaces
+{
+    public interface IPaymentsRepository
+    {
+        public IActionResult CreatePayment();
+        public IActionResult GetPayments();
+        public IActionResult GetPayment(Guid paymentId);
+        public IActionResult UpdatePayment(Guid paymentId);
+        public IActionResult DeletePayment(Guid paymentId);
+    }
+}
