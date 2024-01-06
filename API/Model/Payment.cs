7aa495e2f7793610f29dc94d5a74e1dd1f8f0c95
adomas-vensas <adoma@DESKTOP-09F4KST>
2024-01-06 17:22:56 +0200

    - Created Payment Class

--- API/Model/Payment.cs
+++ API/Model/Payment.cs
@@ -5,12 +5,10 @@ namespace API.Model
 {
     public class Payment
     {
-        [Required]
         public Guid Id { get; set; }
         public Guid OrderId { get; set; }
         public PaymentType PaymentType { get; set; }
         public PaymentState PaymentState { get; set; }
-        public DateTime Date { get; set; }
-
+        public DateTime LastUpdated { get; set; }
     }
 }
