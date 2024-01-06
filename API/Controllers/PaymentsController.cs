18260fa1b68c8a37ab1542514adf4cc640669446
adomas-vensas <adoma@DESKTOP-09F4KST>
2024-01-06 17:23:54 +0200

    - Added a Payments Controller

---
+++ API/Controllers/PaymentsController.cs
@@ -0,0 +1,87 @@
+﻿using API.DTOs;
+using API.Model;
+using API.Repositories.Interfaces;
+using API.Requests.Payments;
+using API.Responses.Payments;
+using API.Services.Interfaces;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("payments")]
+    public class PaymentsController : Controller
+    {
+        private readonly IPaymentService _paymentsService;
+        private readonly IMapper _mapper;
+
+        public PaymentsController(IPaymentService paymentsRepository, IMapper mapper)
+        {
+            _paymentsService = paymentsRepository;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        public IActionResult CreatePayment(CreatePaymentRequest request)
+        {
+            var paymentDomain = _mapper.Map<Payment>(request.Payment);
+            var createdPayment = _paymentsService.CreatePayment(paymentDomain);
+            var response = new CreatePaymentResponse(_mapper.Map<PaymentDTO>(createdPayment));
+
+            return Ok(response);
+        }
+
+        [HttpGet]
+        public IActionResult GetPayments()
+        {
+            var payments = _paymentsService.GetPayments();
+            var response = new GetPaymentsResponse(_mapper.Map<List<PaymentDTO>>(payments));
+
+            return Ok(response);
+        }
+
+        [HttpGet("{paymentId}")]
+        public IActionResult GetPayment([FromRoute] GetPaymentRequest request)
+        {
+            var paymentDomain = _paymentsService.GetPayment(request.PaymentId);
+            
+            if(paymentDomain == null)
+            {
+                return NotFound();
+            }
+
+            var response = new GetPaymentsResponse(_mapper.Map<List<PaymentDTO>>(paymentDomain));
+            
+            return Ok(response);
+        }
+
+        [HttpPut("{paymentId}")]
+        public IActionResult UpdatePayment(UpdatePaymentRequest request, Guid paymentId)
+        {
+            var payment = _mapper.Map<Payment>(request.Payment);
+            bool success = _paymentsService.UpdatePayment(paymentId, payment);
+
+            if(!success)
+            {
+                return NotFound();
+            }
+
+            return Ok();
+        }
+
+        [HttpDelete("{paymentId}")]
+        public IActionResult DeletePayment(DeletePaymentRequest request)
+        {
+            bool success = _paymentsService.DeletePayment(request.PaymentId);
+
+            if(!success)
+            {
+                return BadRequest();
+            }
+
+            return Ok();
+        }
+
+    }
+}
