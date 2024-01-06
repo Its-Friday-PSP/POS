4cba14974df143a078774d381759ebe03cb4097c
adomas-vensas <adoma@DESKTOP-09F4KST>
2024-01-06 17:23:16 +0200

    - Created PaymentDTO

---
+++ API/DTOs/PaymentDTO.cs
@@ -0,0 +1,18 @@
+﻿using API.Enumerators;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs
+{
+    public class PaymentDTO
+    {
+        [Required]
+        public Guid Id { get; set; }
+        [Required]
+        public Guid OrderId { get; set; }
+        [Required]
+        public PaymentType PaymentType { get; set; }
+        [Required]
+        public PaymentState PaymentState { get; set; }
+        public DateTime? LastUpdated { get; set; }
+    }
+}
