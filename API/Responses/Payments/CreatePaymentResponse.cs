ea9bd4703d31aea28d11890ae97e04f4b3293845
adomas-vensas <adoma@DESKTOP-09F4KST>
2024-01-06 17:22:43 +0200

    - Created Payment Responses

---
+++ API/Responses/Payments/CreatePaymentResponse.cs
@@ -0,0 +1,15 @@
+﻿using API.DTOs;
+
+namespace API.Responses.Payments
+{
+    public class CreatePaymentResponse
+    {
+        public PaymentDTO Payment { get; set; }
+
+        public CreatePaymentResponse(PaymentDTO payment)
+        {
+            Payment = payment;
+        }
+
+    }
+}
