ef83161010fb3a5588a33fc2d775d480fc050cc2
Simukas1 <simasjarukaitis@gmail.com>
2024-01-07 02:52:49 +0200

    add [required]

--- API/DTOs/EmployeeDTO.cs
+++ API/DTOs/EmployeeDTO.cs
@@ -5,11 +5,15 @@ namespace API.DTOs
 {
     public class EmployeeDTO
     {
+        [Required]
         public Guid Id { get; set; }
         [Required]
         public Auth Auth { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Role { get; set; }
+        [Required]
         public List<ServiceTimeSlots> ServiceTimeSlots { get; set; }
     }
 }
